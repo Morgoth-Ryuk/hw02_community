5d5e75bfad578b47788ba1b558db16eb33b68cf2
Anastasiya Cherkasova <cherkasovaam@bk.ru>
2023-03-24 03:10:10 +0300

    4 sprint decorate

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
+from django.shortcuts import redirect
 
 
 QUANTITY_OF_POSTS_ON_PAGE = 10
+
+
+def authorized_only(func):
+    # Функция-обёртка в декораторе может быть названа как угодно
+    def check_user(request, *args, **kwargs):
+        # В любую view-функцию первым аргументом передаётся объект request,
+        # в котором есть булева переменная is_authenticated,
+        # определяющая, авторизован ли пользователь.
+        if request.user.is_authenticated:
+            # Возвращает view-функцию, если пользователь авторизован.
+            return func(request, *args, **kwargs)
+        # Если пользователь не авторизован — отправим его на страницу логина.
+        return redirect('/auth/login/')        
+    return check_user
 
 
 def index(request):
@@ -14,6 +29,7 @@ def index(request):
     return render(request, 'posts/index.html', context)
 
 
+@authorized_only
 def groups(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
