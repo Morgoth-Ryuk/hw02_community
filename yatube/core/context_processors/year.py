9dad70158f9de4da28c43da8d49f81118960c7ae
Anastasiya Cherkasova <cherkasovaam@bk.ru>
2023-03-25 01:29:07 +0300

    4 sprint decorate3

---
+++ yatube/core/context_processors/year.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+def year(request):
+    """Добавляет переменную с текущим годом."""
+
+    today = datetime.today()
+    year = today.year
+    return {
+        'year': year,
+    }
