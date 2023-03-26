52c1d76be635a3001608f4ecd95e42e41e6ba92e
Anastasiya Cherkasova <cherkasovaam@bk.ru>
2023-03-26 18:11:42 +0300

    pagenator

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Post, Group
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 
 
@@ -20,24 +21,48 @@ def authorized_only(func):
         return redirect('/auth/login/')        
     return check_user
 
-@login_required
+#@login_required
 def index(request):
-    posts = Post.objects.order_by(
-        '-pub_date')[:QUANTITY_OF_POSTS_ON_PAGE]
-    context = {
-        'posts': posts,
-    }
-    return render(request, 'posts/index.html', context)
+    post_list = Post.objects.all().order_by('-pub_date')
+    # Если порядок сортировки определен в классе Meta модели,
+    # запрос будет выглядеть так:
+    # post_list = Post.objects.all()
+    # Показывать по 10 записей на странице.
+    paginator = Paginator(post_list, 10) 
 
+    # Из URL извлекаем номер запрошенной страницы - это значение параметра page
+    page_number = request.GET.get('page')
+
+    # Получаем набор записей для страницы с запрошенным номером
+    page_obj = paginator.get_page(page_number)
+    # Отдаем в словаре контекста
+    context = {
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/index.html', context) 
 
 @authorized_only
+#def groups(request, slug):
+    #template = 'posts/group_list.html'
+    #group = get_object_or_404(Group, slug=slug)
+    #posts = group.posts.order_by(
+        #'-pub_date')[:QUANTITY_OF_POSTS_ON_PAGE]
+    #context = {
+        #'group': group,
+        #'posts': posts,
+    #}
+    #return render(request, template, context)
+
 def groups(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.order_by(
-        '-pub_date')[:QUANTITY_OF_POSTS_ON_PAGE]
+    post_list = group.posts.all().order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
         'group': group,
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, template, context)
